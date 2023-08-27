0a33775cab6baf4f291bcf1eb773adc586aa61fe
Obi Fortune <gabrielobi.of@gmail.com>
2023-08-27 11:30:46 +0100

    added the mantine setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,35 @@
-import { type AppType } from "next/app";
-import { api } from "~/utils/api";
-import "~/styles/globals.css";
+import { MantineProvider } from '@mantine/core';
+import { type AppType } from 'next/app';
+import Head from 'next/head';
+import { RouterTransition } from '~/components/RouterTransition';
+import '~/styles/globals.css';
+import { api } from '~/utils/api';
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType = props => {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'light',
+        }}
+      >
+        <RouterTransition />
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
