9a763eae042a9e279e4313dcbaaaf6e5b88932bf
Obi Fortune <gabrielobi.of@gmail.com>
2023-08-27 19:00:55 +0100

    switch to playwright

--- src/utils/testHelpers.ts
+++ src/utils/testHelpers.ts
@@ -1,49 +1,7 @@
-import puppeteer from 'puppeteer';
-const minimal_args = [
-  '--autoplay-policy=user-gesture-required',
-  '--disable-background-networking',
-  '--disable-background-timer-throttling',
-  '--disable-backgrounding-occluded-windows',
-  '--disable-breakpad',
-  '--disable-client-side-phishing-detection',
-  '--disable-component-update',
-  '--disable-default-apps',
-  '--disable-dev-shm-usage',
-  '--disable-domain-reliability',
-  '--disable-extensions',
-  '--disable-features=AudioServiceOutOfProcess',
-  '--disable-hang-monitor',
-  '--disable-ipc-flooding-protection',
-  '--disable-notifications',
-  '--disable-offer-store-unmasked-wallet-cards',
-  '--disable-popup-blocking',
-  '--disable-print-preview',
-  '--disable-prompt-on-repost',
-  '--disable-renderer-backgrounding',
-  '--disable-setuid-sandbox',
-  '--disable-speech-api',
-  '--disable-sync',
-  '--hide-scrollbars',
-  '--ignore-gpu-blacklist',
-  '--metrics-recording-only',
-  '--mute-audio',
-  '--no-default-browser-check',
-  '--no-first-run',
-  '--no-pings',
-  '--no-sandbox',
-  '--no-zygote',
-  '--password-store=basic',
-  '--use-gl=swiftshader',
-  '--use-mock-keychain',
-  '--shm-size=3gb',
-];
+import playwright from 'playwright';
 
 export const getBrowser = async () => {
-  const browser = await puppeteer.launch({
-    headless: 'new',
-    args: minimal_args,
-    ignoreHTTPSErrors: true,
-  });
+  const browser = await playwright.chromium.launch();
 
   return browser;
 };
