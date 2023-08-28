bc61c637f9c72ac265d58b3c7de9b26b088ccddc
Obi Fortune <gabrielobi.of@gmail.com>
2023-08-28 04:51:23 +0100

    completed form for add member

--- src/pages/repoAddMembers.tsx
+++ src/pages/repoAddMembers.tsx
@@ -1,46 +1,28 @@
 import {
+  Anchor,
   Button,
+  Code,
   Container,
   Group,
   Select,
   Stack,
+  Text,
   TextInput,
   Textarea,
-  createStyles,
-  rem,
+  Title,
 } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import CustomError from '~/components/Error';
 import CustomSuccess from '~/components/Success';
 import { api } from '~/utils/api';
 
-const useStyles = createStyles(theme => ({
-  root: {
-    position: 'relative',
-  },
-
-  input: {
-    height: rem(54),
-    paddingTop: rem(18),
-  },
-
-  label: {
-    position: 'absolute',
-    pointerEvents: 'none',
-    fontSize: theme.fontSizes.xs,
-    paddingLeft: theme.spacing.sm,
-    paddingTop: `calc(${theme.spacing.sm} / 2)`,
-    zIndex: 1,
-  },
-}));
+const Permissions = ['pull', 'triage', 'push', 'maintain', 'admin'];
 
 const Page = () => {
   const DELIMITER = ',';
 
   const repoFetch = api.repoTools.repoFetch.useMutation();
   const addUsers = api.repoTools.addUsers.useMutation();
-
-  const { classes } = useStyles();
   const form = useForm({
     initialValues: {
       token: '',
@@ -50,28 +32,43 @@ const Page = () => {
     initialValues: {
       selectedRepo: '',
       users: '',
+      permission: 'pull',
     },
     validate: {
-      selectedRepo: value => value.length > 0,
+      selectedRepo: value =>
+        value.length > 0 ? false : 'Please select a repository',
       users: value => {
         const users = value.split(DELIMITER).map(user => user.trim());
-        return users.length > 0 && users.every(user => user.length > 0);
+        const invalidUsers = users.filter(user => user.length === 0);
+
+        return invalidUsers.length > 0
+          ? 'Please enter a valid list of users'
+          : false;
       },
     },
   });
 
   return (
     <Container py="xl">
+      <Title>Github Repo Collab tool</Title>
+      <Text c="dimmed">
+        Github limits you to 50 invitations for every 24 hour period -{' '}
+        <Anchor href="https://docs.github.com/en/rest/collaborators/collaborators?apiVersion=2022-11-28#add-a-repository-collaborator">
+          src
+        </Anchor>
+      </Text>
       <Stack>
         <form onSubmit={form.onSubmit(values => repoFetch.mutate(values))}>
           <Stack>
-            {repoFetch.isError && <CustomError />}
-
             <TextInput
               withAsterisk
-              classNames={classes}
               label="Token"
               placeholder="Github Token"
+              description={
+                <Text>
+                  Token should have <Code color="grape">read:user, repo</Code>
+                </Text>
+              }
               {...form.getInputProps('token')}
             />
             <Group position="right">
@@ -79,40 +76,55 @@ const Page = () => {
                 Submit
               </Button>
             </Group>
+            {repoFetch.isError && (
+              <CustomError message={repoFetch.error.message} />
+            )}
           </Stack>
         </form>
 
         {repoFetch.data != undefined && (
           <form
             onSubmit={updateForm.onSubmit(values => {
-              const users = values.users
+              const { users, ...others } = values;
+              const mappedUsers = users
                 .split(DELIMITER)
                 .map(user => user.trim());
 
               addUsers.mutate({
-                selectedRepo: values.selectedRepo,
                 token: form.values.token,
-                users,
+                users: mappedUsers,
+                ...others,
               });
             })}
           >
             <Stack>
               <Select
                 withAsterisk
-                classNames={classes}
                 label="Repository to update"
                 placeholder="Pick one"
                 data={repoFetch.data}
                 {...updateForm.getInputProps('selectedRepo')}
+              />
+              <Select
+                withAsterisk
+                label="Set Permission"
+                placeholder="Pick one"
+                data={Permissions}
+                {...updateForm.getInputProps('permission')}
               />
               <Textarea
                 withAsterisk
                 autosize
                 minRows={2}
                 maxRows={4}
-                label="Emails/Usernames"
+                label="Usernames"
                 placeholder="Enter Users"
-                description={`Enter a list of emails or usernames separated by a '${DELIMITER}'`}
+                description={
+                  <Text>
+                    Enter a list of usernames separated by a{' '}
+                    <Code color="grape">{DELIMITER}</Code>
+                  </Text>
+                }
                 {...updateForm.getInputProps('users')}
               />
               <Group position="right">
@@ -122,7 +134,7 @@ const Page = () => {
               </Group>
 
               {addUsers.isError && (
-                <CustomError message="Something went wrong!" />
+                <CustomError message={addUsers.error.message} />
               )}
               {addUsers.isSuccess && (
                 <CustomSuccess message="Users added successfully!" />
