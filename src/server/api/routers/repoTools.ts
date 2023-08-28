73e9374e91d233e604dc4381c36d88b5c47e6bf3
Obi Fortune <gabrielobi.of@gmail.com>
2023-08-28 04:51:06 +0100

    setup add member logic

--- src/server/api/routers/repoTools.ts
+++ src/server/api/routers/repoTools.ts
@@ -23,10 +23,45 @@ export const repoToolsRouter = createTRPCRouter({
     .input(
       z.object({
         selectedRepo: z.string(),
-        users: z.string(),
+        users: z.array(z.string()),
+        token: z.string(),
+        permission: z.string(),
       }),
     )
-    .mutation(({}) => {
-      return {};
+    .mutation(async ({ input }) => {
+      try {
+        const { permission } = input;
+
+        const octokit = new Octokit({ auth: input.token });
+        const [owner, repo] = input.selectedRepo.split('/');
+
+        if (owner == undefined || repo == undefined) {
+          throw new Error('Invalid repository name');
+        }
+
+        const handleCollaboratorAdd = async (username: string) => {
+          return await octokit.rest.repos.addCollaborator({
+            owner,
+            repo,
+            username,
+            permission,
+          });
+        };
+
+        await Promise.all(input.users.map(handleCollaboratorAdd));
+      } catch (error) {
+        console.error(error);
+
+        if (
+          error != null &&
+          typeof error == 'object' &&
+          'message' in error &&
+          typeof error.message == 'string'
+        ) {
+          throw new Error(error.message);
+        }
+
+        throw new Error('An unknown error occurred');
+      }
     }),
 });
