ca5d5e24105c6791eefa7e3285f08fc62aa111a8
Obi Fortune <gabrielobi.of@gmail.com>
2023-09-13 00:24:40 +0100

    setup link accordion

---
+++ src/components/LinksGroup.tsx
@@ -0,0 +1,146 @@
+import {
+  Box,
+  Collapse,
+  Group,
+  Text,
+  ThemeIcon,
+  UnstyledButton,
+  createStyles,
+  getStylesRef,
+  rem,
+} from '@mantine/core';
+import { IconChevronLeft, IconChevronRight } from '@tabler/icons-react';
+import { useEffect, useState } from 'react';
+
+const useStyles = createStyles(theme => ({
+  control: {
+    fontWeight: 500,
+    display: 'block',
+    width: '100%',
+    padding: `${theme.spacing.xs} ${theme.spacing.md}`,
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+    fontSize: theme.fontSizes.sm,
+
+    '&:hover': {
+      backgroundColor:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[7]
+          : theme.colors.gray[0],
+      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    },
+  },
+
+  link: {
+    fontWeight: 500,
+    display: 'block',
+    textDecoration: 'none',
+    padding: `${theme.spacing.xs} ${theme.spacing.md}`,
+    paddingLeft: rem(31),
+    marginLeft: rem(30),
+    fontSize: theme.fontSizes.sm,
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[0]
+        : theme.colors.gray[7],
+    borderLeft: `${rem(1)} solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+
+    '&:hover': {
+      backgroundColor:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[7]
+          : theme.colors.gray[0],
+      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    },
+  },
+
+  linkActive: {
+    '&, &:hover': {
+      backgroundColor: theme.fn.variant({
+        variant: 'light',
+        color: theme.primaryColor,
+      }).background,
+      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
+        .color,
+      [`& .${getStylesRef('icon')}`]: {
+        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
+          .color,
+      },
+    },
+  },
+
+  chevron: {
+    transition: 'transform 200ms ease',
+  },
+}));
+
+export interface LinksGroupProps {
+  icon: React.FC<{ size?: string }>;
+  label: string;
+  initiallyOpened?: boolean;
+  links?: { label: string; link: string }[];
+}
+
+export const LinksGroup = ({
+  icon: Icon,
+  label,
+  initiallyOpened,
+  links,
+}: LinksGroupProps) => {
+  const { classes, theme, cx } = useStyles();
+  const [opened, setOpened] = useState(initiallyOpened ?? false);
+  const [active, setActive] = useState('');
+  useEffect(() => {
+    if (typeof window == undefined) return;
+
+    const currPath = window.location.pathname.slice(1).toLowerCase();
+    setActive(currPath);
+  }, []);
+
+  const hasLinks = Array.isArray(links);
+  const ChevronIcon = theme.dir === 'ltr' ? IconChevronRight : IconChevronLeft;
+  const items = (hasLinks ? links : []).map(link => (
+    <Text<'a'>
+      component="a"
+      className={cx(classes.link, {
+        [classes.linkActive]: link.link === active,
+      })}
+      href={`/${link.link}`}
+      key={link.label}
+    >
+      {link.label}
+    </Text>
+  ));
+
+  return (
+    <>
+      <UnstyledButton
+        onClick={() => setOpened(o => !o)}
+        className={classes.control}
+      >
+        <Group position="apart" spacing={0}>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <ThemeIcon variant="light" size={30}>
+              <Icon size="1.1rem" />
+            </ThemeIcon>
+            <Box ml="md">{label}</Box>
+          </Box>
+          {hasLinks && (
+            <ChevronIcon
+              className={classes.chevron}
+              size="1rem"
+              stroke={1.5}
+              style={{
+                transform: opened
+                  ? `rotate(${theme.dir === 'rtl' ? -90 : 90}deg)`
+                  : 'none',
+              }}
+            />
+          )}
+        </Group>
+      </UnstyledButton>
+      {hasLinks ? <Collapse in={opened}>{items}</Collapse> : null}
+    </>
+  );
+};
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,13 @@
 import {
-  ActionIcon,
   Navbar,
+  ScrollArea,
   Text,
   createStyles,
-  getStylesRef,
   rem,
-  type ActionIconProps,
-  type CSSObject,
   type NavbarProps,
 } from '@mantine/core';
-import {
-  IconBoxMultiple1,
-  IconCircleNumber1,
-  IconHexagonNumber1,
-  IconRosetteNumber1,
-  IconSquareNumber1,
-} from '@tabler/icons-react';
-import { useEffect, useState } from 'react';
+import { IconNumber1, IconNumber2 } from '@tabler/icons-react';
+import { LinksGroup } from './LinksGroup';
 
 const useStyles = createStyles(theme => ({
   footer: {
@@ -27,133 +18,51 @@ const useStyles = createStyles(theme => ({
     }`,
   },
 
-  link: {
-    ...(theme.fn.focusStyles() as Record<string, CSSObject>),
-    display: 'flex',
-    alignItems: 'center',
-    textDecoration: 'none',
-    fontSize: theme.fontSizes.sm,
-    color:
-      theme.colorScheme === 'dark'
-        ? theme.colors.dark[1]
-        : theme.colors.gray[7],
-    padding: `${theme.spacing.xs} ${theme.spacing.sm}`,
-    borderRadius: theme.radius.sm,
-    fontWeight: 500,
-
-    '&:hover': {
-      backgroundColor:
-        theme.colorScheme === 'dark'
-          ? theme.colors.dark[6]
-          : theme.colors.gray[0],
-      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-
-      [`& .${getStylesRef('icon')}`]: {
-        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-      },
-    },
-  },
-
-  linkIcon: {
-    ref: getStylesRef('icon'),
-    color:
-      theme.colorScheme === 'dark'
-        ? theme.colors.dark[2]
-        : theme.colors.gray[6],
-    marginRight: theme.spacing.sm,
-  },
-
-  linkActive: {
-    '&, &:hover': {
-      backgroundColor: theme.fn.variant({
-        variant: 'light',
-        color: theme.primaryColor,
-      }).background,
-      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
-        .color,
-      [`& .${getStylesRef('icon')}`]: {
-        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
-          .color,
-      },
-    },
+  links: {
+    marginLeft: `calc(${theme.spacing.md} * -1)`,
+    marginRight: `calc(${theme.spacing.md} * -1)`,
   },
 }));
 
+const pageGenerate = (
+  page: string,
+  icon: React.FC<{ size?: string }>,
+  opened = false,
+) => {
+  return [
+    {
+      label: `Stage ${page}`,
+      icon,
+      initiallyOpened: opened,
+      links: [
+        { link: `stage${page}/upload`, label: `Stage ${page} Upload` },
+        { link: `stage${page}/pending`, label: `Stage ${page} Pending` },
+        { link: `stage${page}/failed`, label: `Stage ${page} Failed` },
+        {
+          link: `stage${page}/not-promoted`,
+          label: `Stage ${page} Results - Not Promoted`,
+        },
+        { link: `stage${page}/results`, label: `Stage ${page} Results` },
+      ],
+    },
+  ];
+};
+
 const data = [
-  {
-    link: 'stage1/upload',
-    label: 'Stage 1 Upload',
-    icon: (props: ActionIconProps) => (
-      <ActionIcon {...props}>
-        <IconCircleNumber1 stroke={2} />
-      </ActionIcon>
-    ),
-  },
-  {
-    link: 'stage1/pending',
-    label: 'Stage 1 Pending',
-    icon: (props: ActionIconProps) => (
-      <ActionIcon {...props}>
-        <IconHexagonNumber1 stroke={2} />
-      </ActionIcon>
-    ),
-  },
-  {
-    link: 'stage1/failed',
-    label: 'Stage 1 Failed',
-    icon: (props: ActionIconProps) => (
-      <ActionIcon {...props}>
-        <IconRosetteNumber1 stroke={2} />
-      </ActionIcon>
-    ),
-  },
-  {
-    link: 'stage1/not-promoted',
-    label: 'Stage 1 Results - Not Promoted',
-    icon: (props: ActionIconProps) => (
-      <ActionIcon {...props}>
-        <IconSquareNumber1 stroke={2} />
-      </ActionIcon>
-    ),
-  },
-  {
-    link: 'stage1/results',
-    label: 'Stage 1 Results',
-    icon: (props: ActionIconProps) => (
-      <ActionIcon {...props}>
-        <IconBoxMultiple1 stroke={2} />
-      </ActionIcon>
-    ),
-  },
+  ...pageGenerate('1', IconNumber1, true),
+  ...pageGenerate('2', IconNumber2),
 ];
 
 const CustomNavbar = (props: Omit<NavbarProps, 'children'>) => {
-  const { classes, cx } = useStyles();
-  const [active, setActive] = useState('');
-
-  useEffect(() => {
-    if (typeof window == undefined) return;
-
-    const currPath = window.location.pathname.slice(1).toLowerCase();
-    setActive(currPath);
-  }, []);
-
-  const links = data.map(item => (
-    <a
-      className={cx(classes.link, {
-        [classes.linkActive]: item.link.toLowerCase() === active,
-      })}
-      href={`/${item.link}`}
-      key={item.label}
-    >
-      <item.icon className={classes.linkIcon} />
-      <span>{item.label}</span>
-    </a>
-  ));
+  const { classes } = useStyles();
 
   return (
-    <Navbar p="md" {...props}>
-      <Navbar.Section grow>{links}</Navbar.Section>
+    <Navbar p="xs" {...props}>
+      <Navbar.Section grow component={ScrollArea} className={classes.links}>
+        {data.map(item => (
+          <LinksGroup {...item} key={item.label} />
+        ))}
+      </Navbar.Section>
 
       <Navbar.Section className={classes.footer}>
         <Text align="center">NOT built with ❤️ anymore</Text>
